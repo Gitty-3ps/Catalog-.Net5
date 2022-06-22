4811d78ee919e0ba770f9bb147c432e0780cd074
Deron Hutchinson <deronhutchoffcial77@gmail.com>
2022-06-22 15:08:54 -0500

    GetItemAsync_WithExistingItem_ReturnsExpectedItem

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -55,6 +55,48 @@ namespace Catalog.UnitTests
             Assert.IsType<NotFoundResult>(result.Result);
         }
 
+           [Fact]
+        public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+        {
+        // Arrange
+        var expectedItem = CreateRandomItem();
+
+        repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(expectedItem);
+
+        
+        var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+        // Act
+        var result = await controller.GetItemAsync(Guid.NewGuid());
+
+        // Assert
+        //    result.Value.Should().BeEquivalentTo(
+        //     expectedItem,
+        //     options => options.ComparingByMembers<Item>());
+
+        Assert.IsType<ItemDto>(result.Value);
+        var dto = (result as ActionResult<ItemDto>).Value;
+        Assert.Equal(expectedItem.Id, dto.Id);
+        Assert.Equal(expectedItem.Name, dto.Name);
+       
+        }
+
+        
+
+        private Item CreateRandomItem()
+        {
+            return new()
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString(),
+                Price = rand.Next(1000),
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+        }
+
+
+
      
     }
 }
