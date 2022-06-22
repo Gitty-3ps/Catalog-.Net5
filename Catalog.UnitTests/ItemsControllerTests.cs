b2f8cbc0582ed1ec97380a34a00af54a0b898b13
Deron Hutchinson <deronhutchoffcial77@gmail.com>
2022-06-21 22:49:51 -0500

    AddedNuGet packages for unit testing

---
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -0,0 +1,16 @@
+namespace Catalog.UnitTests
+{
+
+    public class ItemsControllerTests
+{
+    [Fact] // UnitofWork_StateUnderTest_ExpectedBehaviour : Naming convention
+    public void GetItemAsync_WithUnexistingItem_ReturnsNotFound()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert  
+    }
+}
+}
